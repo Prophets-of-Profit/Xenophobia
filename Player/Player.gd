f07ad4a6a1c508cafc00d4ed1a69ca11a0b99e29
Saurabh Totey <SaurabhTotey@gmail.com>
2019-04-13 23:31:14 -0600

    Implemented Player collision with Fences

--- Player/Player.gd
+++ Player/Player.gd
@@ -1,11 +1,14 @@
 extends KinematicBody2D
 
-const MAX_SPEED = 300
+const MAX_SPEED = 750
+var movementVelocity: Vector2 = Vector2(0, 0)
 
 func _physics_process(delta: float) -> void:
-	var movementVelocity: Vector2 = Vector2(Input.get_action_strength("move_right") - Input.get_action_strength("move_left"), Input.get_action_strength("move_down") - Input.get_action_strength("move_up"))
+	move_and_slide(movementVelocity)
+
+func _input(event):
+	movementVelocity = Vector2(Input.get_action_strength("move_right") - Input.get_action_strength("move_left"), Input.get_action_strength("move_down") - Input.get_action_strength("move_up"))
 	movementVelocity *= MAX_SPEED
 	if movementVelocity.length_squared() > pow(MAX_SPEED, 2):
 		movementVelocity = movementVelocity.normalized() * MAX_SPEED
 	get_node("PlayerSprite").setSprite(movementVelocity)
-	move_and_slide(movementVelocity)
