87680149d403479b87451b81b96a657e68bff3b5
Saurabh Totey <SaurabhTotey@gmail.com>
2019-04-16 14:09:50 -0600

    Shrunk player collider to allow for navigation through tilemaps

--- Player/Player.gd
+++ Player/Player.gd
@@ -3,6 +3,11 @@ extends KinematicBody2D
 const MAX_SPEED = 750
 var movementVelocity: Vector2 = Vector2(0, 0)
 
+func _ready():
+	var playerCollider = ($PlayerCollider.shape as RectangleShape2D)
+	playerCollider.extents.x -= 4
+	playerCollider.extents.y -= 4
+
 func _physics_process(delta: float) -> void:
 	move_and_slide(movementVelocity)
 
@@ -11,4 +16,4 @@ func _input(event):
 	movementVelocity *= MAX_SPEED
 	if movementVelocity.length_squared() > pow(MAX_SPEED, 2):
 		movementVelocity = movementVelocity.normalized() * MAX_SPEED
-	get_node("PlayerSprite").setSprite(movementVelocity)
+	$PlayerSprite.setSprite(movementVelocity)
